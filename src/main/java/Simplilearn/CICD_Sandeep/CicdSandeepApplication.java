8c3a9bdc1ceff18d12d6ffd015ffa042994d3e48
Sandeep0408 <sandeepkumar41993@yahoo.in>
2021-06-04 01:27:55 +0530

    Adding .war file to git repo CICD

---
+++ src/main/java/Simplilearn/CICD_Sandeep/CicdSandeepApplication.java
@@ -0,0 +1,13 @@
+package Simplilearn.CICD_Sandeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSandeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSandeepApplication.class, args);
+	}
+
+}
